0aa8eb765408dd22cf03dce4b21c7aba4c9f1e61
abdur-shobur <abdurshobur.developer@gmail.com>
2023-04-15 12:54:37 +0600

    add new-dash-line

---
+++ new-dash-line/script.js
@@ -0,0 +1,47 @@
+//  final 1
+window.addEventListener('scroll', function () {
+	let l = Path_440.getTotalLength();
+	let dasharray = l;
+	let dashoffset = l;
+	e = document.documentElement;
+	theFill.setAttributeNS(null, 'stroke-dasharray', l);
+	theFill.setAttributeNS(null, 'stroke-dashoffset', l);
+	dashoffset = l - (window.scrollY * l) / (e.scrollHeight - e.clientHeight);
+	console.log(
+		'window.scrollY',
+		window.scrollY,
+		'scrollTop',
+		e.scrollTop,
+		'scrollHeight',
+		e.scrollHeight,
+		'clientHeight',
+		e.clientHeight,
+		'dash-offset',
+		dashoffset
+	);
+	theFill.setAttributeNS(null, 'stroke-dashoffset', dashoffset);
+});
+
+// final 2
+window.addEventListener('scroll', function () {
+	let l = Path_4402.getTotalLength();
+	let dasharray = l;
+	let dashoffset = l;
+	e = document.documentElement;
+	theFill.setAttributeNS(null, 'stroke-dasharray', l);
+	theFill.setAttributeNS(null, 'stroke-dashoffset', l);
+	dashoffset = l - (window.scrollY * l) / (e.scrollHeight - e.clientHeight);
+	console.log(
+		'window.scrollY',
+		window.scrollY,
+		'scrollTop',
+		e.scrollTop,
+		'scrollHeight',
+		e.scrollHeight,
+		'clientHeight',
+		e.clientHeight,
+		'dash-offset',
+		dashoffset
+	);
+	theFill.setAttributeNS(null, 'stroke-dashoffset', dashoffset);
+});
